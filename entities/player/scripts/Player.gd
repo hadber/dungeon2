00102ec7ce3d365fa53d0ad94226626bf89734ea
ionutx54 <i.ionut977@yahoo.com>
2020-07-18 01:17:33 +0300

    smooth movement added

--- entities/player/scripts/Player.gd
+++ entities/player/scripts/Player.gd
@@ -1,8 +1,30 @@
 extends KinematicBody2D
 
-var speed = 400  # speed in pixels/sec
-var velocity = Vector2.ZERO
+# var speed = 400  # speed in pixels/sec
+# var velocity = Vector2.ZERO
 
+var MAX_SPEED = 400
+var ACCELERATION = 2000
+var motion = Vector2() # motion on the cartesian plane
+
+
+func get_input_axis():
+	var axis = Vector2.ZERO
+	axis.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
+	axis.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	return axis.normalized() # prevent moving faster on diagonal than on cartesian directions
+
+func apply_friction(amount):
+	if motion.length() > amount:
+		motion -= motion.normalized() * amount 
+	else:
+		motion = Vector2.ZERO
+
+func apply_movement(acceleration):
+	motion += acceleration
+	motion = motion.clamped(MAX_SPEED)
+	
+"""
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('ui_right'):
@@ -15,7 +37,14 @@ func get_input():
 		velocity.y -= 1
 	# Make sure diagonal movement isn't faster
 	velocity = velocity.normalized() * speed
+"""
 
 func _physics_process(delta):
-	get_input()
-	velocity = move_and_slide(velocity)
+	var axis = get_input_axis()
+	if(axis == Vector2.ZERO):
+		apply_friction(ACCELERATION * delta)
+	else:
+		apply_movement(axis * ACCELERATION * delta)
+	motion = move_and_slide(motion)
+	# get_input()
+	# velocity = move_and_slide(velocity)
