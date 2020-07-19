c7a27c03d28900ab21240bcf5b15bbe000bfd019
hadber <adber@tuta.io>
2020-07-20 01:15:41 +0200

    movement good

--- entities/player/scripts/Player.gd
+++ entities/player/scripts/Player.gd
@@ -10,8 +10,8 @@ var motion = Vector2() # motion on the cartesian plane
 
 func get_input_axis():
 	var axis = Vector2.ZERO
-	axis.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
-	axis.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	axis.x = Input.get_action_strength("right") - Input.get_action_strength("left") 
+	axis.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	return axis.normalized() # prevent moving faster on diagonal than on cartesian directions
 
 func apply_friction(amount):
@@ -23,28 +23,19 @@ func apply_friction(amount):
 func apply_movement(acceleration):
 	motion += acceleration
 	motion = motion.clamped(MAX_SPEED)
-	
-"""
-func get_input():
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	# Make sure diagonal movement isn't faster
-	velocity = velocity.normalized() * speed
-"""
 
 func _physics_process(delta):
 	var axis = get_input_axis()
 	if(axis == Vector2.ZERO):
-		apply_friction(ACCELERATION * delta * 30)
+		apply_friction(ACCELERATION * delta)
+	elif(axis.x == 0):
+		motion.x -= motion.normalized().x * ACCELERATION * delta
+		apply_movement(axis * ACCELERATION * delta)
+	elif(axis.y == 0):
+		motion.y -= motion.normalized().y * ACCELERATION * delta
+		apply_movement(axis * ACCELERATION * delta)
 	else:
 		apply_movement(axis * ACCELERATION * delta)
 	motion = move_and_slide(motion)
-	# get_input()
-	# velocity = move_and_slide(velocity)
+#	print(motion)
+
