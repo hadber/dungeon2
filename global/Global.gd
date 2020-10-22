daff677f091e6454c6a802d6fc108cbbcaeb4755
snow <snow@andromeda.lan>
2020-10-23 00:41:36 +0200

    can get avatar + minor lobby work

---
+++ global/Global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+#var ONLINE = Steam.loggedOn()
+var STEAM_ID = Steam.getSteamID()
+#var OWNED = Steam.isSubscribed()
+
+func _ready():
+
+	var INIT = Steam.steamInit()
+	print("Steam init: " + str(INIT))
+		
+	if INIT['status'] != 1:
+		print("Failed to initialize Steam. " + str(INIT['verbal']) + " Shutting down...")
+		get_tree().quit()
+
+func _process(_delta):
+	Steam.run_callbacks()
