a88279243d66141157c6cb96ea49aeeb6a30b6cc
hadber <adber@tuta.io>
2020-12-13 16:15:01 +0100

    slime

--- Scripts/Floor1/Room.gd
+++ Scripts/Floor1/Room.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const PlayerScene = preload("res://Scenes/Player.tscn")
+const SlimeScene = preload("res://Scenes/Slime.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,6 +13,10 @@ func _ready():
 	Player1.get_node("CenterContainer/Name").text = Steam.getFriendPersonaName(Global.STEAM_ID)
 	self.add_child_below_node($ball_text, Player1)
 	Global.PLAYERS["host"] = Player1
+	
+	self.add_child_below_node($ball_text, SlimeScene.instance())
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -19,4 +19,25 @@ func _process(_delta):
 	background.position.x = x
  
 func _ready():
+	# warning-ignore:return_value_discarded
+	$MarginContainer/CenterContainer/VBoxContainer/CenterContainer/VBoxContainer/btPlay.connect("pressed", self, "_btOnPlayPressed")
+	# warning-ignore:return_value_discarded
+	$MarginContainer/CenterContainer/VBoxContainer/CenterContainer/VBoxContainer/btOptions.connect("pressed", self, "_btOnOptionsPressed")
+	# warning-ignore:return_value_discarded
+	$MarginContainer/CenterContainer/VBoxContainer/CenterContainer/VBoxContainer/btQuit.connect("pressed", self, "_btOnQuitPressed")
+	
 	$lVersion.text = Global.version
+
+func _btOnPlayPressed():
+	print("Start the game (go to the main area)")
+	# warning-ignore:return_value_discarded
+	var ret = get_tree().change_scene("res://Scenes/Room.tscn") # test scene
+	if(ret == OK):
+		print('test')
+	
+func _btOnOptionsPressed():
+	print("No options at the moment :( (sorry!)")
+
+func _btOnQuitPressed():
+	print("Goodbye!")
+	get_tree().quit()
