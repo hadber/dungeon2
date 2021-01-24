74c095d9d57da0aa1ef5940a2650a85c09ab4331
hadber <adber@tuta.io>
2021-01-24 18:43:04 +0100

    mp 1

--- Scripts/Networking/MultiplayerLoader.gd
+++ Scripts/Networking/MultiplayerLoader.gd
@@ -1,7 +1,6 @@
 extends Node
 
+const mp_scene = preload("res://Scenes/Multiplayer.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#load the multiplayer scene in here
-	pass
-
+	add_child(mp_scene.instance())
